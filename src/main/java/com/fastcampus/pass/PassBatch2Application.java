01504493b36cba3fe159971d7739eb0dd36aa057
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-11-16 17:22:49 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/pass/PassBatch2Application.java
@@ -0,0 +1,13 @@
+package com.fastcampus.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassBatch2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassBatch2Application.class, args);
+    }
+
+}
