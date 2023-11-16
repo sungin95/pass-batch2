2531964b21d8e5b782fd3325a759a3939c4756ad
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-11-16 17:24:54 +0900

    다시 시작
    기존의 프로젝트를 initialize 에서 다운로드 받아 압축 해제하는 방식으로 만들었는데. 알 수 없는 에러가 자꾸 떠서 새롭게 만듬.
    03. JPA를 사용한 MYSQL 연동까지 따라옴.

---
+++ src/main/java/com/fastcampus/pass/repository/booking/BookingEntity.java
@@ -0,0 +1,32 @@
+package com.fastcampus.pass.repository.booking;
+
+import com.fastcampus.pass.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "booking")
+public class BookingEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 DB에 위임합니다. (AUTO_INCREMENT)
+    private Integer bookingSeq;
+    private Integer passSeq;
+    private String userId;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+    private boolean usedPass;
+    private boolean attended;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+    private LocalDateTime cancelledAt;
+
+}
