a129cdd35cfa2e6731a11fb3c10b37ba8fa64a0f
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-11-20 12:36:01 +0900

    05 Batch 이용권 일괄 지급

--- src/main/java/com/fastcampus/pass/repository/pass/PassRepository.java
+++ src/main/java/com/fastcampus/pass/repository/pass/PassRepository.java
@@ -1,10 +1,6 @@
 package com.fastcampus.pass.repository.pass;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-
-import javax.transaction.Transactional;
 
 public interface PassRepository extends JpaRepository<PassEntity, Integer> {
 }
