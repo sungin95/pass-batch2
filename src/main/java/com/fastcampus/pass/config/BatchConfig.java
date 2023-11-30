bf9eb1e9a0d0691cd1811dfcd182a100c4c70454
Sungin Lee <100878663+sungIn95@users.noreply.github.com>
2023-11-30 14:41:37 +0900

    의미 없는 변화

--- src/main/java/com/fastcampus/pass/config/BatchConfig.java
+++ src/main/java/com/fastcampus/pass/config/BatchConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
  * JobRepository, JobLauncher, JobRegistry, PlatformTransactionManager, JobBuilderFactory, StepBuilderFactory 빈으로 제공됩니다.
  * https://docs.spring.io/spring-batch/docs/current/api/org/springframework/batch/core/configuration/annotation/EnableBatchProcessing.html
  */
+
 @EnableBatchProcessing
 @Configuration
 public class BatchConfig {
--- src/main/java/com/fastcampus/pass/repository/packaze/PackageEntity.java
+++ src/main/java/com/fastcampus/pass/repository/packaze/PackageEntity.java
@@ -1,4 +1,4 @@
-package com.fastcampus.pass.repository.packaze;
+package com.fastcampus.pass.repository.packaze; // package는 reserved word로 사용할 수 없어서 packaze를 사용합니다.
 
 import com.fastcampus.pass.repository.BaseEntity;
 import lombok.Getter;
@@ -14,12 +14,11 @@ import javax.persistence.*;
 @Table(name = "package")
 public class PackageEntity extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 DB에 위임합니다. (AUTO_INCREMENT)
     private Integer packageSeq;
 
     private String packageName;
     private Integer count;
     private Integer period;
-
 
 }
